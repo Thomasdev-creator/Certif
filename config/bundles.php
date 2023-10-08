197edf39cf5ec55dac5fb5093da3d05f5a9501f8
Thomas Fournier <thomasfournierbrest@gmail.com>
2023-10-08 12:46:51 +0200

    correct problem database, install frontend

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
---
+++ migrations.sql
@@ -0,0 +1,6 @@
+[notice] Migrating (dry-run) up to DoctrineMigrations\Version20231004160837
+[notice] finished in 3.7ms, used 20M memory, 3 migrations executed, 18 sql queries
+
+ [OK] Successfully migrated to version : DoctrineMigrations\Version20231004160837          
+
+
